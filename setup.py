ab6631cabe118ac09e9280983c1e882566dac09a
Rich Wareham <rjw57@cam.ac.uk>
2015-02-26 13:16:48 +0000

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bdfu",
-    version="0.1",
+    version="1.0.0",
     packages=find_packages(),
     entry_points=dict(
         console_scripts=[
